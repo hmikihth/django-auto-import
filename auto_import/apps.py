573103c49de78b190582e4ceca12735e0e3f75c9
Miklos Horvath <79752640+hmikihth@users.noreply.github.com>
2024-06-22 11:43:27 +0100

    Fix app name in apps.py

--- auto_import/apps.py
+++ auto_import/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AutoIncludeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auto_include'
+    name = 'auto_import'
